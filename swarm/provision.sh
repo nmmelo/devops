bf9b7e90082b92215f7d55b37ad2da7cb976d1cb
Nuno Melo <ncjweb@msn.com>
2022-05-16 22:04:57 +0100

    3º alteração do dia 16 de maio de 2022

--- swarm/provision.sh
+++ swarm/provision.sh
@@ -8,3 +8,11 @@ sudo systemctl enable docker
 sudo curl -L "https://github.com/docker/compose/releases/download/1.25.5/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 sudo chmod +x /usr/local/bin/docker-compose
 sudo ln -s /usr/local/bin/docker-compose /usr/bin/docker-compose
+#yum install -y net-tool*
+#yum install -y update
+cat <<EOT >> /etc/hosts
+10.2.1.2 manager
+10.2.1.3 worker1
+10.2.1.4 worker2
+EOT
+
